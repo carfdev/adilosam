95d0dfeee37f414dd763e66561e3e763078b82fc
carfdev <jose.carlos@fdez.se>
2024-03-22 17:29:56 +0100

    add choose section in hemstadning.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from 'astro-robots-txt';
+import robotsConfig from './robots-txt.config';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://elaborate-sprite-90d43d.netlify.app/',
+  integrations: [
+    tailwind(), 
+    robotsTxt(robotsConfig), 
+    sitemap()
+  ]
 });
---
+++ robots-txt.config.ts
@@ -0,0 +1,15 @@
+import type { RobotsTxtOptions } from 'astro-robots-txt';
+import siteConfig from './site.config';
+
+const robotsConfig: RobotsTxtOptions = {
+  policy: [
+    {
+      userAgent: '*',
+      // The next line enables or disables the crawling on the `robots.txt` level
+      disallow: siteConfig.disableIndexing ? '/' : '',
+    },
+  ],
+  sitemapBaseFileName: 'sitemap-index', // default 'sitemap-index'
+};
+
+export default robotsConfig;
